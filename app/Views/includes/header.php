bd7ca060018d70373e1d4642bc1b7154cab2107b
shubham agrawal <145680136+shubhamimg@users.noreply.github.com>
2024-12-22 15:35:45 +0530

    design changes

--- app/Views/includes/header.php
+++ app/Views/includes/header.php
@@ -9,8 +9,29 @@
       <meta property="og:url" content="">
       <meta property="og:image" content="">
        <!-- Favicon -->
-        <link rel="shortcut icon" type="image/x-icon" href="<?= base_url('assets/imgs/theme/favicon.png');?>">
+        <!-- <link rel="shortcut icon" type="image/x-icon" href="<?= base_url('assets/imgs/theme/favicon.png');?>"> -->
       <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/mainc619.css'); ?>">
       <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/plugins/owl-carousel/owl.carousel.css'); ?>">
       <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/plugins/owl-carousel/owl.theme.css'); ?>">
+
+
+
+
+      <link rel="apple-touch-icon" sizes="57x57" href="<?= base_url('assets/imgs/theme//apple-icon-57x57.png');?>">
+<link rel="apple-touch-icon" sizes="60x60" href="<?= base_url('assets/imgs/theme//apple-icon-60x60.png');?>">
+<link rel="apple-touch-icon" sizes="72x72" href="<?= base_url('assets/imgs/theme//apple-icon-72x72.png');?>">
+<link rel="apple-touch-icon" sizes="76x76" href="<?= base_url('assets/imgs/theme//apple-icon-76x76.png');?>">
+<link rel="apple-touch-icon" sizes="114x114" href="<?= base_url('assets/imgs/theme//apple-icon-114x114.png');?>">
+<link rel="apple-touch-icon" sizes="120x120" href="<?= base_url('assets/imgs/theme//apple-icon-120x120.png');?>">
+<link rel="apple-touch-icon" sizes="144x144" href="<?= base_url('assets/imgs/theme//apple-icon-144x144.png');?>">
+<link rel="apple-touch-icon" sizes="152x152" href="<?= base_url('assets/imgs/theme//apple-icon-152x152.png');?>">
+<link rel="apple-touch-icon" sizes="180x180" href="<?= base_url('assets/imgs/theme//apple-icon-180x180.png');?>">
+<link rel="icon" type="image/png" sizes="192x192"  href="<?= base_url('assets/imgs/theme//android-icon-192x192.png');?>">
+<link rel="icon" type="image/png" sizes="32x32" href="<?= base_url('assets/imgs/theme//favicon-32x32.png');?>">
+<link rel="icon" type="image/png" sizes="96x96" href="<?= base_url('assets/imgs/theme//favicon-96x96.png');?>">
+<link rel="icon" type="image/png" sizes="16x16" href="<?= base_url('assets/imgs/theme//favicon-16x16.png');?>">
+<link rel="manifest" href="<?= base_url('assets/imgs/theme//manifest.json');?>">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="<?= base_url('/ms-icon-144x144.png');?>">
+<meta name="theme-color" content="#ffffff">
 </head>
